ed5df7251559fc08d458aee7bbf3caf7dc08249b
msramyams <ramyams998@gmail.com>
2022-03-27 01:01:11 +0530

    first JPA project

---
+++ JPAproject/src/com/cg/jpacurd/dao/StudentDao.java
@@ -0,0 +1,12 @@
+package com.cg.jpacurd.dao;
+
+import com.cg.jpacurd.entities.Student;
+	public interface StudentDao 
+	{
+	public abstract Student getStudentById(int id);
+	public abstract void addStudent(Student student);
+	public abstract void removeStudent(Student student);
+	public abstract void updateStudent(Student student);
+	public abstract void commitTransaction();
+	public abstract void beginTransaction();
+}
